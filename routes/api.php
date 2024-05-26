ded8046c4c37111ba56954fa1a9598155642de7e
etziony0202 <etziowan@gmail.com>
2024-05-26 07:38:18 +0530

    Modify api.php

--- routes/api.php
+++ routes/api.php
@@ -83,7 +83,7 @@ Route::get('/stock-sub-categories', function (Request $request) {
     foreach ($sub_categories as $sub_category) {
         $data[] = [
             'id' => $sub_category->id,
-            'text' => $sub_category->name_text . " (" . $sub_category->measuring_unit . ")",
+            'text' => $sub_category->name_text . " (" . $sub_category->measurement_unit . ")",
         ];
     }
 
